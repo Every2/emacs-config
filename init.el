e9764d23f47eef8ddba10f69babfb2cac9176b60
Every <christiansantoslima21@gmail.com>
2024-09-27 14:23:26 -0300

    new configuration

--- init.el
+++ init.el
@@ -108,16 +108,4 @@
 				   :program "path"
 				   :cwd "path"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vterm magit dap-mode alchemist elixir-mode rustic tree-sitter-langs tree-sitter company company-mode flycheck-eglot flycheck which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
